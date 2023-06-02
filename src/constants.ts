f1bffa66789ac459ea5d753f713e84c7d63d2aa1
Arcadio Pernia <arcadio.perniac@gmail.com>
2023-06-01 23:35:23 -0400

    :construction: refactor index & about pages

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Experience new places, meet amazing people & have fun on your mountain bike';
+export const SITE_DESCRIPTION = 'Ride your mountain bike, share with the community & live a stoked life';
+export const SITE_SUBTITLE = 'Plan your ride today and join our community.';
