ae63ea8b0ca534919fc3778a4b546e28c76e583d
Arcadio Pernia <arcadio.perniac@gmail.com>
2023-06-05 23:24:09 -0400

    :recycle: refactor topRatedSpots component & about page

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,28 @@
 export const SITE_TITLE = 'Experience new places, meet amazing people & have fun on your mountain bike';
 export const SITE_DESCRIPTION = 'Ride your mountain bike, share with the community & live a stoked life';
 export const SITE_SUBTITLE = 'Plan your ride today and join our community.';
+export const ABOUT_TITLE = "About US";
+
+export const PLACES = [
+  {
+    title: "Icalma",
+    postUrl: "/posts/post-1/",
+    location: "Lonquimay, 🇨🇱",
+    imageUrl: "/icalma.png",
+    amenities: ["bed", "outdoor_grill", "local_cafe", "kayaking"]
+  },
+  {
+    title: "Bikepark Nevados de Chillán",
+    postUrl: "/posts/post-2/",
+    location: "Pinto, 🇨🇱",
+    imageUrl: "/chillan.jpeg",
+    amenities: ["bed", "park", "restaurant"]
+  },
+  {
+    title: "Cerro Bayo",
+    postUrl: "/posts/post-3/",
+    location: "Villa La Angostura, 🇦🇷",
+    imageUrl: "/villa-angostura.jpeg",
+    amenities: ["bed", "park", "restaurant", "local_gas_station"]
+  }
+];
